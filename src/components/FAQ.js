b249f15de28dc50c32a39cce016f048c22efb167
Muhammad Shaheryar Haider <shaheryarjerry@gmail.com>
2021-09-08 22:58:43 +0500

    Add Questions

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -2,8 +2,12 @@ import './App.css'
 
 const FAQ = () => {
     return (
-        <div className= 'AppFAQ'>
+        <div>
+       <div>
             <img className='AppImg' src='https://pngimg.com/uploads/netflix/netflix_PNG3.png' />
+       </div>
+       <div className= 'AppFAQ'>
+           
             <div className='AppBody'>
             <p className='para'> &#60; back</p>
             <h1 className='h1'style= {{marginTop:'-11px'}}>
@@ -16,7 +20,7 @@ const FAQ = () => {
                     <select className ='input'>
                              <option >Multiple Choice</option>
                      </select>
-                    <button className ='input' style={{backgroundColor:'#5765EE',color:'#DBDCE0',fontWeight:'bold',borderRadius:'10px', fontFamily:'Arial, Tahoma, sans-serif'}}>Add Question</button> 
+                    <button className ='input' style={{backgroundColor:'#5765EE',color:'#DBDCE0',fontWeight:'bold',borderRadius:'10px'}}>Add Question</button> 
                     </div>
                     <div className='inputFirstLine'>
                     <input className='input' type='text' placeholder='Enter Choices' />
@@ -25,8 +29,44 @@ const FAQ = () => {
                 </div>
                 <button className= 'buttomButton'>Save and Continue</button>
             </div>
+            <div className = 'FAQRight' style={{backgroundColor:'white'}}>
+              <div className = 'bodyRight'>
+               <div className='h1Right'>
+                   <h1>
+                       Here are the questions in your survey
+                            </h1>
+                <div className='questions'>
+                   <div className ='question'>
+                                <p className='questionNo'>1</p>
+                       <p className='questionText'>How would you feel if you could no longer use Tesla?</p>
+                            </div>
+                            <div className ='question'>
+                                <p className='questionNo'>2</p>
+                       <p className='questionText'>What would you see if Tesla were no longer available?</p>
+                            </div>
+                            <div className ='question'>
+                                <p className='questionNo'>3</p>
+                       <p className='questionText'>What is the main benifit you receive from Tesla?</p>
+                            </div>
+                            <div className ='question'>
+                                <p className='questionNo'>4</p>
+                       <p className='questionText'>What type of people do you think would most benifits from Tesla?</p>
+                            </div>
+                            <div className ='question'>
+                                <p className='questionNo'>5</p>
+                       <p className='questionText'>How can we improve Tesla for you?</p>
+                            </div>
+                            <div className ='question'>
+                                <p className='questionNo'>6</p>
+                       <p className='questionText'>What is your job title?</p>
+                                </div>
+                                </div>
+               </div>
+               </div>
+            </div>
             
            
+        </div>
         </div>
     )
 }
